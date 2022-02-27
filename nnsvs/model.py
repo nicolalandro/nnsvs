49012281fc3049841dbd705080b299e589bedc3e
Ryuichi Yamamoto <zryuichi@gmail.com>
2022-02-27 20:58:19 +0900

    No encoder tacotron2

--- nnsvs/model.py
+++ nnsvs/model.py
@@ -392,3 +392,68 @@ class NoAttTacotron2(BaseModel):
             torch.Tensor: the output
         """
         return self.forward(seq, lengths, None)
+
+
+class NoAttTacotron2NoEncoder(BaseModel):
+    """Tacotron2 decoder without attention"""
+
+    def __init__(
+        self,
+        in_dim=512,
+        out_dim=80,
+        decoder_layers=2,
+        decoder_hidden_dim=1024,
+        decoder_prenet_layers=2,
+        decoder_prenet_hidden_dim=256,
+        decoder_prenet_dropout=0.5,
+        decoder_zoneout=0.1,
+        postnet_layers=5,
+        postnet_channels=512,
+        postnet_kernel_size=5,
+        postnet_dropout=0.5,
+        reduction_factor=1,
+    ):
+        super().__init__()
+        self.decoder = NoAttTacotron2Decoder(
+            in_dim,
+            out_dim,
+            decoder_layers,
+            decoder_hidden_dim,
+            decoder_prenet_layers,
+            decoder_prenet_hidden_dim,
+            decoder_prenet_dropout,
+            decoder_zoneout,
+            reduction_factor,
+        )
+        self.postnet = Tacotron2Postnet(
+            out_dim,
+            postnet_layers,
+            postnet_channels,
+            postnet_kernel_size,
+            postnet_dropout,
+        )
+
+    def is_autoregressive(self):
+        return True
+
+    def forward(self, inputs, in_lens, decoder_targets):
+        outs = self.decoder(inputs, in_lens, decoder_targets)
+
+        outs_fine = outs + self.postnet(outs)
+
+        # (B, C, T) -> (B, T, C)
+        outs = outs.transpose(2, 1)
+        outs_fine = outs_fine.transpose(2, 1)
+
+        return outs_fine
+
+    def inference(self, seq, lengths=None):
+        """Inference step
+
+        Args:
+            seq (torch.Tensor): input sequence
+
+        Returns:
+            torch.Tensor: the output
+        """
+        return self.forward(seq, lengths, None)
